157dc17bec0bd3b0d882116dfafca9a43ffe6041
Donna Ann Issac <donna.ann1209@gmail.com>
2018-03-06 11:54:24 +0530

    Add name optionally and button to the comment form

--- index.js
+++ index.js
@@ -7,21 +7,50 @@ function createContainer () {
   return containerBox
 }
 
+function removePlaceHolder (event) {
+  event.target.placeholder = ''
+}
+
+function addPlaceHolder (event, text) {
+  event.target.placeholder = text
+}
+
 function createInputBox () {
   let inputBox = document.createElement('input')
-  inputBox.setAttribute('contenteditable', 'true')
+  inputBox.id = 'inputBox'
   inputBox.setAttribute('placeholder', 'Enter comment here')
-  inputBox.addEventListener('focus', (event) => {
-    event.target.placeholder = ''
-  })
-  inputBox.addEventListener('blur', (event) => {
-    event.target.placeholder = 'Enter comment here'
-  })
+  inputBox.addEventListener('focus', (event) => removePlaceHolder(event))
+  inputBox.addEventListener('blur', (event) => addPlaceHolder(event, 'Enter comment here'))
   return inputBox
 }
+
+function createNameHolder () {
+  let authorName = document.createElement('input')
+  authorName.setAttribute('placeholder', 'Name (Optional)')
+  authorName.addEventListener('focus', (event) => removePlaceHolder(event))
+  authorName.addEventListener('blur', (event) => addPlaceHolder(event, 'Name (Optional)'))
+  return authorName
+}
+
+function createSubmitBtn () {
+  let submitBtn = document.createElement('button')
+  submitBtn.innerText = 'Submit'
+  submitBtn.addEventListener('click', () => { console.log('Hello World') })
+  return submitBtn
+}
+
+function createIpWrapper () {
+  let authorDetails = document.createElement('div')
+  authorDetails.appendChild(createNameHolder())
+  authorDetails.appendChild(createSubmitBtn())
+  return authorDetails
+}
+
 function createCommentForm (container) {
   let commentForm = document.createElement('div')
-  container.appendChild(commentForm.appendChild(createInputBox()))
+  commentForm.appendChild(createInputBox())
+  commentForm.appendChild(createIpWrapper())
+  container.appendChild(commentForm)
 }
 
 createCommentForm(createContainer())
