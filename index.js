28658005cd98e72971bbb1e3656119d2c5663189
Donna Ann Issac <donna.ann1209@gmail.com>
2018-03-06 11:31:04 +0530

    Create container with input box

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('Hello World')
+let $ = id => document.getElementById(id)
+
+function createContainer () {
+  let containerBox = document.createElement('div')
+  containerBox.id = 'container'
+  $('comments').appendChild(containerBox)
+  return containerBox
+}
+
+function createInputBox () {
+  let inputBox = document.createElement('input')
+  inputBox.setAttribute('contenteditable', 'true')
+  inputBox.setAttribute('placeholder', 'Enter comment here')
+  inputBox.addEventListener('focus', (event) => {
+    event.target.placeholder = ''
+  })
+  inputBox.addEventListener('blur', (event) => {
+    event.target.placeholder = 'Enter comment here'
+  })
+  return inputBox
+}
+function createCommentForm (container) {
+  let commentForm = document.createElement('div')
+  container.appendChild(commentForm.appendChild(createInputBox()))
+}
+
+createCommentForm(createContainer())
