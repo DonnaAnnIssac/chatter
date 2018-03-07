04559002ded876f55593b9f6a1d2c2607092d7dd
Donna Ann Issac <donna.ann1209@gmail.com>
2018-03-07 15:54:29 +0530

    Connect to postgres and serve static files on server

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require('express')
+const config = require('./config')
+const pg = require('pg')
 const app = express()
 
-app.listen(8000, () => {
+let connString = 'pg://' + config.db.user + ':' + config.db.password + '@' + config.db.host + ':' + config.db.port + '/' + config.db.name
+let pgPool = new pg.Pool({
+  connectionString: connString
+})
+
+pgPool.connect((err, client) => {
+  if (err) console.log('Unable to connect: ' + err)
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+    next()
+  })
+  app.use(express.static('public'))
+})
+app.listen(config.app.port, () => {
   console.log('Listening on port 8000')
 })
---
+++ config.js
@@ -0,0 +1,14 @@
+const config = {
+  app: {
+    port: 8000
+  },
+  db: {
+    user: 'postgres',
+    password: 'hellohello0!',
+    host: 'localhost',
+    port: 5432,
+    name: 'chatter'
+  }
+}
+
+module.exports = config
